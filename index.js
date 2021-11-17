83d7fb7fda4c7f1357be310c45fc783b1c8f9337
notchioma <paullois829@gmail.com>
2021-11-17 15:30:07 +0100

    glint update

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-$('.sleek').not('.slick-initialized').slick({
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  prevArrow: '<span class="privarrow"><i class="fas fa-chevron-left"></i></span>',
-  nextArrow: '<span class="nextarrow"><i class="fas fa-chevron-right"></i></span>',
-});
-
-$(".counter").counterUp({
-      delay: 10,
-      time: 1000
+window.onload = () => {
+  $('.sleek').not('.slick-initialized').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    prevArrow: '<span class="privarrow"><i class="fas fa-chevron-left"></i></span>',
+    nextArrow: '<span class="nextarrow"><i class="fas fa-chevron-right"></i></span>',
   });
+
+  try {
+    $('.counter').counterUp({
+      // duration: 9000,
+      time: 1000,
+      delay: 10
+    });
+  } catch (error) {
+    console.log(error.stack)
+  }
+}
